fdb22a45cd33256ffad71ecc92408c2890ab814f
22502788 <adem.fekihassen77@gmail.com>
2026-02-20 10:54:26 +0100

    ajout des fonction des opperation

---
+++ src/pycalc.py
@@ -0,0 +1,19 @@
+opp1 = input("veuiller rentre le premier opperant : ")
+signe = input("veuiller rentre le signe de lopperation : ")
+
+
+opp2 = input("veuiller rentre le deuxiemme opperant : ")
+
+def mult(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def pow(a,b):
+    return a**b
+
+
