ea9d07b6850b8d9047b338fe7be0df58092b1b72
22502788 <adem.fekihassen77@gmail.com>
2026-02-20 11:04:23 +0100

    debut de la logique du programme

--- src/pycalc.py
+++ src/pycalc.py
@@ -18,8 +18,24 @@ while True :
     print("1 : effectuer une operation ")
     print("2 : quitter laplication ")
     choix= int(input("les choix son selectionner avec des numero veuiller rentre votre choix : "))
-    opp1 = int(input("veuiller rentre le premier opperant : "))
-    signe = input("veuiller rentre le signe de lopperation : ")
-    opp2 = int(input("veuiller rentre le deuxiemme opperant : "))
 
+    if choix == 1 :
+        opp1 = int(input("veuiller rentre le premier opperant : "))
+        signe = input("veuiller rentre le signe de lopperation : ")
+        opp2 = int(input("veuiller rentre le deuxiemme opperant : "))
 
+        if signe == "+":
+            print(add(opp1,opp2))
+
+        if signe == "-":
+            print(add(opp1,opp2))
+
+        if signe == "x" or signe == "X" or signe == "*":
+            print(mult(opp1,opp2))
+        if signe == "+":
+            print(add(opp1,opp2))
+        if signe == "+":
+            print(add(opp1,opp2))
+
+    if choix == 2 :
+        break
