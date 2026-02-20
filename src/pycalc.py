30a1c722ddcf625b7920b0122c4f921f0c687561
22502788 <adem.fekihassen77@gmail.com>
2026-02-20 11:08:00 +0100

    essaise nvl affichage des resultat

--- src/pycalc.py
+++ src/pycalc.py
@@ -12,12 +12,16 @@ def sub(a,b):
 def pow(a,b):
     return a**b
 
+print("bienvenue dans la calulatrice ")
+
 while True :
-    print("bienvenue dans la calulatrice ")
+
     print ('_________________________________________________________________')
     print("1 : effectuer une operation ")
     print("2 : quitter laplication ")
     choix= int(input("les choix son selectionner avec des numero veuiller rentre votre choix : "))
+    print ('_________________________________________________________________')
+
 
     if choix == 1 :
         opp1 = int(input("veuiller rentre le premier opperant : "))
@@ -25,17 +29,19 @@ while True :
         opp2 = int(input("veuiller rentre le deuxiemme opperant : "))
 
         if signe == "+":
-            print(add(opp1,opp2))
+            print(opp1, " + ", opp2, " = ",add(opp1,opp2))
 
         if signe == "-":
-            print(add(opp1,opp2))
+            print(sub(opp1,opp2))
 
         if signe == "x" or signe == "X" or signe == "*":
             print(mult(opp1,opp2))
-        if signe == "+":
-            print(add(opp1,opp2))
-        if signe == "+":
-            print(add(opp1,opp2))
+
+        if signe == "/":
+            print(div(opp1, opp2))
+
+        if signe == "**":
+            print(pow(opp1,opp2))
 
     if choix == 2 :
         break
